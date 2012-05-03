4b7ea324aa10db2b720401ce9d2a913d4c7c219d
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-05-03 16:01:25 +0200

    init

---
+++ textAdEngineDemo_client/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace textAdEngineDemo_client
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ textAdEngineDemo_server/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace textAdEngineDemo_server
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+		}
+	}
+}
