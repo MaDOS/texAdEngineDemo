23c50764aec9e70eb8de00ae5231de45176c632a
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-05-22 18:49:59 +0200

    - some testing

--- textAdEngineDemo_client/main.cs
+++ textAdEngineDemo_client/main.cs
@@ -16,9 +16,25 @@ namespace textAdEngineDemo_client
 		private static string username;
 		private static string password;
 		private static bool loggedIn = false;
+		private static view cView;
 		
 		public static void Main(string[] args)
 		{
+//			for(int i = 1; i < 40; i++)
+//			{
+//				Console.Write("*");
+//				Console.Write("*");
+//				Console.Write("*");
+//				Console.Write("*");
+//				Console.Write("*");
+//				Console.Write("*");
+//				Console.Write("*");
+//				Console.Write("*");
+//				Console.Write("*");
+//				Console.Write("#");
+//			}
+//			Console.WriteLine(((int)Math.Ceiling((double)"".Length / (double)70)).ToString());
+//			Console.ReadKey();
 			connect();
 		}
 		
@@ -38,7 +54,20 @@ namespace textAdEngineDemo_client
 			Console.Write("\n\r");
 			
 			Console.Write("Password: ");
-			password = Console.ReadLine();
+			ConsoleKeyInfo pressed = Console.ReadKey(true);
+			while(pressed.Key != ConsoleKey.Enter)
+			{
+				Console.Write("*");
+				if(pressed.Modifiers != ConsoleModifiers.Shift)
+				{
+					password += pressed.Key.ToString().ToLower();
+				}
+				pressed = Console.ReadKey(true);
+			}
+			
+			Console.Write("\n\r");
+			
+			cView = new view();
 			
 			serverEndPoint = new IPEndPoint(serverIP, port);
             server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -59,7 +88,6 @@ namespace textAdEngineDemo_client
 	        while(server.Connected)
 	        {
 	        	Thread.Sleep(20);
-	        	Console.Write("@" + serverIP.ToString() + "> ");
 	        	string command = Console.ReadLine();
 	        	if(server.Connected)
 	        	{
@@ -111,10 +139,27 @@ namespace textAdEngineDemo_client
 						Console.WriteLine("Logged in sucessfully");
 					}
 				}	
+				else if(dataPair[0] == "#health")
+				{
+					cView.changeHealth(dataPair[1]);
+				}
+				else if(dataPair[0] == "#area")
+				{
+					cView.changeArea(dataPair[1]);
+					cView.WriteText("** Your are now in area: " + dataPair[1], ConsoleColor.Black, ConsoleColor.Green);
+				}
+				else if(dataPair[0] == "#areaDesc")
+				{
+					cView.WriteText(dataPair[1], ConsoleColor.Black, ConsoleColor.White);
+				}
+				else if(dataPair[0] == "#chat")
+				{
+					cView.WriteText("'' " + dataPair[1] +  " said: " + dataPair[1], ConsoleColor.Black, ConsoleColor.Red);
+				}
 			}
 			else if(received.Trim() != "")
 			{
-				Console.WriteLine("server said: " + received);
+				cView.WriteText("server said: " + received, ConsoleColor.Blue, ConsoleColor.White);
 			}
 			
 		}
