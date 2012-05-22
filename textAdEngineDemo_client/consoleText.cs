704032714c056e8114b4751c70aabb0355fcbce9
Robin Bechtel-Ostmann <rbechtel.ostmann@googlemail.com>
2012-05-22 18:49:15 +0200

    Class wich handles text and bg+fore-color of a text

---
+++ textAdEngineDemo_client/consoleText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace textAdEngineDemo_client
+{
+	public class consoleText
+	{
+		public string text;
+		public ConsoleColor foreColor = ConsoleColor.White;
+		public ConsoleColor backColor = ConsoleColor.Black;
+		
+		public consoleText(string text_param, ConsoleColor foreColor_param, ConsoleColor backColor_param)
+		{
+			text = text_param;
+			foreColor = foreColor_param;
+			backColor = backColor_param;
+		}
+		
+		public consoleText()
+		{
+			text = "";
+			foreColor = ConsoleColor.White;
+			backColor = ConsoleColor.Black;
+		}
+	}
+}
